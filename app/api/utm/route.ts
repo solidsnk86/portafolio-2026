b0b703e62d8a7c21725be88c9e3c9167824dc3ab
solidsnk86 <calcagni.gabriel86@gmail.com>
2026-06-16 08:58:01 -0300

    ealry return (api)

--- app/api/utm/route.ts
+++ app/api/utm/route.ts
@@ -3,7 +3,9 @@ import { supabase } from "@/utils/supabase";
 export async function POST(req: Request) {
   const source = await req.json();
   const { utm, referer, lastAccessId } = source;
-
+  if (!utm || !lastAccessId) {
+    return Response.json({ message: "Apa la lata, falta un parámetro!" }, { status: 400 });
+  }
   try {
     const { error } = await supabase
       .from("solidsnk_collection_utm")
