0aa416fe259c2c9af614e3767f5b2a48801fa6da
solidsnk86 <calcagni.gabriel86@gmail.com>
2026-06-23 20:57:45 -0300

    fix status code

--- app/api/collection/cv-views/route.ts
+++ app/api/collection/cv-views/route.ts
@@ -22,7 +22,7 @@ export async function POST(req: Request) {
 
     if (viewsError) return Response.json({ message: viewsError.message });
 
-    return Response.json({}, { status: 204 });
+    return Response.json({}, { status: 200 });
   } catch (error) {
     return Response.json(
       { message: "Error en el servidor", error: (error as TypeError).message },
