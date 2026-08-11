c49b42e01a045b76cca71b5849e08e4dca0ec326
solidsnk86 <calcagni.gabriel86@gmail.com>
2026-08-11 17:23:05 -0300

    chore: btn outline offset added

--- components/common/button.tsx
+++ components/common/button.tsx
@@ -5,7 +5,7 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   function Button({ className = "", type = "button", ...props }, ref) {
     const baseClasses = (className =
-      "inline-flex items-center justify-center px-6 py-3 bg-foreground text-background font-semibold transition-colors hover:outline-4 hover:outline-indigo-500");
+      "inline-flex items-center justify-center px-6 py-3 bg-foreground text-background font-semibold transition-colors hover:outline-4 hover:outline-indigo-500/70 outline-offset-2");
 
     return (
       <button
